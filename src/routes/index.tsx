93d9e632a6e36b6667b038f5cee9767c5126d65a
wmarquesv <walace.mv@hotmail.com>
2026-05-18 03:03:40 -0300

    adicionado rotas para usuários de diferentes role

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -5,15 +5,31 @@ import { Loading } from "../components/Loading";
 import { EmployeeRoutes } from "./EmployeeRoutes";
 import { ManagerRoutes } from "./ManagerRoutes";
 
-const isLoading = true;
+const isLoading = false;
+
+const session = {
+  user: {
+    role: "manager",
+  },
+};
 
 export function Routes() {
+  function Route() {
+    switch (session?.user.role) {
+      case "employee":
+        return <EmployeeRoutes />;
+      case "manager":
+        return <ManagerRoutes />;
+      default:
+        return <AuthRoutes />;
+    }
+  }
   if (isLoading) {
     return <Loading />;
   }
   return (
     <BrowserRouter>
-      <AuthRoutes />
+      <Route />
     </BrowserRouter>
   );
 }
