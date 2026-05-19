e2552ffffd4e66251eafb07430e263cd783119ec
wmarquesv <walace.mv@hotmail.com>
2026-05-18 22:45:25 -0300

    requisição de signIn adicionada

--- src/pages/SingIn.tsx
+++ src/pages/SingIn.tsx
@@ -1,5 +1,8 @@
 import { useActionState } from "react";
 import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+
+import { api } from "../services/api";
 
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
@@ -19,12 +22,17 @@ export function SingIn() {
         password: formData.get("password"),
       });
 
-      console.log(data);
+      const response = await api.post("/sessions", data);
+      console.log(response.data);
     } catch (error) {
       console.log(error);
 
       if (error instanceof ZodError) {
         return { message: error.issues[0].message };
+      }
+
+      if (error instanceof AxiosError) {
+        return { message: error.response?.data.message };
       }
 
       return { message: "Nao foi possível entrar!" };
