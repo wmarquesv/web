c453b3d577f5ae05153925e8e9747cc39edde6ca
wmarquesv <walace.mv@hotmail.com>
2026-05-18 22:00:26 -0300

    cadastro de usuário a db inserido

--- src/pages/SingUp.tsx
+++ src/pages/SingUp.tsx
@@ -1,5 +1,9 @@
 import { useState } from "react";
 import { z, ZodError } from "zod";
+import { AxiosError } from "axios";
+import { useNavigate } from "react-router";
+
+import { api } from "../services/api";
 
 import { Button } from "../components/Button";
 import { Input } from "../components/Input";
@@ -25,7 +29,9 @@ export function SingUp() {
   const [passwordConfirm, setPasswordConfirm] = useState("");
   const [isLoading, setIsLoading] = useState(false);
 
-  function onSubmit(e: React.FormEvent) {
+  const navigate = useNavigate();
+
+  async function onSubmit(e: React.FormEvent) {
     e.preventDefault();
 
     try {
@@ -37,9 +43,19 @@ export function SingUp() {
         password,
         passwordConfirm,
       });
+
+      await api.post("/users", data);
+
+      if (confirm("Cadastrado com sucesso. Ir para tela de entrar?")) {
+        navigate("/");
+      }
     } catch (error) {
       if (error instanceof ZodError) {
         return alert(error.issues[0].message);
+      }
+
+      if (error instanceof AxiosError) {
+        return alert(error.response?.data.message);
       }
 
       alert("Nao foi possível cadastrar");
